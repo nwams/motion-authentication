86b5525439c92d334369ec89c3dde656576684d3
nwams <ncn35@cornell.edu>
2015-10-20 22:17:42 -0400

    Initial Commit

---
+++ motionAuthentication WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  motionAuthentication WatchKit Extension
+//
+//  Created by Nwamaka Nzeocha on 10/20/15.
+//  Copyright © 2015 Nwamaka Nzeocha. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
